34efe45c841c3a7d9be8b36f8da89f712de45e42
Alvaro Erazo <ader148@hotmail.com>
2021-09-14 02:22:56 -0500

    integracon de mensajes con toastr he implementacion de formulario eventos

--- app/Http/Controllers/EventosController.php
+++ app/Http/Controllers/EventosController.php
@@ -4,14 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\File;
+use Brian2694\Toastr\Facades\Toastr;
+use DateTime;
+use Illuminate\Support\Facades\Storage;
+
 class EventosController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('eventos.home');
     }
 
-    protected function create(){
-        var_dump('hola desde crear');
-        die();
+    protected function create(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|image'
+        ]);
+
+        $imagen = $request->file('file')->store('public/eventos');
+        $url = Storage::url($imagen);
+
+        $nombre = $request->input('nombre_evento');
+        $descripcion = $request->input('descripcion_evento');
+        $latitud = $request->input('txtLat');
+        $longitud = $request->input('txtLng');
+        $fecha = $request->input('fecha_evento');
+        $hora = $request->input('hora_evento');
+
+        $time_input = strtotime("$fecha $hora");
+
+        //$date_input = getDate($time_input);
+        $fecha_formateada = date("Y-m-d H:i:s", $time_input);
+
+        $posicion = $latitud + ',' + $longitud;
+
+
+        /*EventosController::create([
+            '' => '',
+        ]);*/
+
+        Toastr::success('Panal agregado correctamente', '', ["positionClass" => "toast-top-center"]);
+        //return $url;
+        return redirect()->back();
     }
 }
+
+//correr este comendo
+//php artisan storage:link
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -29,6 +29,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+
 </head>
 
 <body onload="initialize();">
@@ -86,6 +88,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!--<script src="js/bootstrap.min.js"></script>-->
     <!--<script src="bootstrap.bundle.min.js"></script>-->
+
+    <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+        <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+        {!! Toastr::message() !!}
 </body>
 
 @include('footer')
