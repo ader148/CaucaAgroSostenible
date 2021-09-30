d50be3bbd0dec33fee06747707eccced575f71ab
kevinalarcon95 <kfalarcon@unicauca.edu.co>
2021-09-29 23:56:09 -0500

    Se le dieron estilos a la vista Agro Oferta

--- resources/views/productos/home.blade.php
+++ resources/views/productos/home.blade.php
@@ -93,7 +93,7 @@
                         </p>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                 </div>
                 </div>
             </div>
